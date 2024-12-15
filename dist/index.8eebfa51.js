0b43893e976c5bb06a224256b346e3bf4f604936
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2024-12-15 19:59:21 +0200

    done deploy fix

---
+++ dist/index.8eebfa51.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",e=>{e.preventDefault(),console.log({name:document.getElementById("name").value,email:document.getElementById("email").value,message:document.getElementById("message").value}),alert("Дякую за підписку на наш сервіс")});
+//# sourceMappingURL=index.8eebfa51.js.map
