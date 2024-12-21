b2672344f67214023dad1f6135be2e4cacf25892
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2024-12-21 17:03:34 +0200

    Updates

---
+++ index.4da762ae.js
@@ -0,0 +1,2 @@
+const e=(e,t,a)=>({name:e,email:t,message:a});document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault(),console.log(e(t.target.elements.name.value,t.target.elements.email.value,t.target.elements.message.value)),alert("Дякую за підписку на наш сервіс")});
+//# sourceMappingURL=index.4da762ae.js.map
